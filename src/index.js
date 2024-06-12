fa9274e2b361e2880627158233504cfc3f93bc48
seungwon <swlee173@gmail.com>
2024-06-12 15:47:42 +0900

    03강 JSX

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Library from "./chapter_03/Library";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Library></Library>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
