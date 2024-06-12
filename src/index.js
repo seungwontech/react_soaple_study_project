583962723355177589ba882020288029ba9fb5b4
seungwon <swlee173@gmail.com>
2024-06-12 17:51:29 +0900

    04강 엘리먼트 렌더링

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { createRoot }  from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Library from "./chapter_03/Library";
 import Clock from "./chapter_04/Clock";
 
-const root = createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 setInterval(() => {
     root.render(
