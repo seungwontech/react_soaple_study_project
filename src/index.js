52f62c9262fd451b4ad94f7a402e803a31ac5199
seungwon <swlee173@gmail.com>
2024-06-20 16:09:57 +0900

    10강 리스트와 키

--- src/index.js
+++ src/index.js
@@ -9,13 +9,14 @@ import CommentList from "./chapter_05/CommentList";
 import Accommodate from "./chapter_07/Accommodate";
 import ConfirmButton from "./chapter_08/ConfirmButton";
 import LandingPage from "./chapter_09/LandingPage";
+import AttendanceBook from "./chapter_10/AttendanceBook";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 setInterval(() => {
     root.render(
         <React.StrictMode>
-            <LandingPage/>
+            <AttendanceBook/>
         </React.StrictMode>,
         document.getElementById('root')
     );
