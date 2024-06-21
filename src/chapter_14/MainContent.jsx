2e7fb0919c5d47aaf3ad93d4bb51efd0e72abbbd
seungwon <swlee173@gmail.com>
2024-06-21 15:02:15 +0900

    14강 컨텍스트

--- src/chapter_14/MainContent.jsx
+++ src/chapter_14/MainContent.jsx
@@ -1,7 +1,7 @@
 import {useContext} from "react";
 import ThemeContext from "./ThemeContext";
 
-function MainContent(props) {
+function MainContent() {
     const {theme, toggleTheme} = useContext(ThemeContext);
 
     return (
@@ -10,8 +10,8 @@ function MainContent(props) {
                 width: "100vw",
                 height: "100vh",
                 padding: "1.5rem",
-                backgroundColor: theme == "light" ? "white" : "black",
-                color: theme == "light" ? "black" : "white",
+                backgroundColor: theme === "light" ? "white" : "black",
+                color: theme === "light" ? "black" : "white",
             }}
         >
             <p>안녕하세요, 테마 변경이 가능한 웹사이트 입니다.</p>
